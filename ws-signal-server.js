e13f03d5ff00e2346b3a0f58dbcc35377a615cc8
Snake174 <jonnysereb@gmail.com>
2025-07-09 21:04:51 +0500

    1

--- ws-signal-server.js
+++ ws-signal-server.js
@@ -4,7 +4,7 @@
 const http = require('http');
 const WebSocket = require('ws');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const PATH = '/ws';
 
 const server = http.createServer();
